ec15754f388d0cd1dc0500089b17956bf044baa0
Lehlogonolo Morgan MOLAODI <lmolaodi@c5r3s8.wethinkcode.co.za>
2019-05-27 14:36:53 +0200

    part one functions

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,23 +6,38 @@
 /*   By: lmolaodi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 13:14:13 by lmolaodi          #+#    #+#             */
-/*   Updated: 2019/05/24 15:48:37 by lmolaodi         ###   ########.fr       */
+/*   Updated: 2019/05/27 14:36:07 by lmolaodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
-#include <stdlib.h>
-#include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char *tempvar;
+	char *dest;
+	char *source;
+	size_t i;
 
-	tempvar = ((char *)malloc(sizeof(*src) * len));
-	if (tempvar == '\0')
-		return (NULL);
-	ft_memcpy(tempvar, src, len);
-	ft_memcpy(dst, tempvar, len);
-	free(tempvar);
+	i = 0;
+	dest = (char *)dst;
+	source = (char *)src;
+
+	if (dest[i] < source[i])
+	{
+		while (i < len)
+		{
+			dest[i] = source[i];
+			i++;
+		}
+	}
+	else
+	{
+		i = len - 1;
+		while (i >= 0)
+		{
+			dest[i] = source[i];
+			i--;
+		}
+	}
 	return (dst);
 }
