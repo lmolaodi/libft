9bf61775a332ad28db639b0464f4588487c110aa
Lehlogonolo Morgan MOLAODI <lmolaodi@c5r3s8.wethinkcode.co.za>
2019-05-27 16:29:22 +0200

    libft library

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lmolaodi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 09:56:33 by lmolaodi          #+#    #+#             */
-/*   Updated: 2019/05/22 16:15:06 by lmolaodi         ###   ########.fr       */
+/*   Updated: 2019/05/27 16:27:56 by lmolaodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,27 @@ int	ft_isalpha(int c);
 int ft_isascii(int c);
 int	ft_isdigit(int c);
 int	ft_isprint(int c);
-char	*ft_strcat(char *restrict s1, const char *restrict s2);
+char	*ft_strcat(char *s1, const char *s2);
 int	ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dest, char *src);
 char	*ft_strdup(const char *s1);
 size_t	ft_strlen(const char *str);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strncpy(char	*dest, const char *src, size_t len);
+char	*ft_strncpy(char *dest, const char *src, size_t len);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
 void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
-char	ft_strstr(const char *haystack, const char *needle);
-void	ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+char	*ft_strstr(const char *haystack, const char *needle);
+void	*ft_memccpy(void *dst, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+char	*ft_strchr(const char *s, int c);
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
