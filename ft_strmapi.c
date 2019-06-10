8b18d6c9514187832f7a0eb68f4b3495c412c8f3
Lehlogonolo Morgan MOLAODI <lmolaodi@c5r10s9.wethinkcode.co.za>
2019-06-10 15:12:48 +0200

    part2 functions

---
+++ ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmolaodi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 11:59:24 by lmolaodi          #+#    #+#             */
+/*   Updated: 2019/06/10 13:29:42 by lmolaodi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	i;
+	
+	if (s && f)
+	{
+		i = 0;
+		str = ft_strnew(ft_strlen(s));
+		while (s[i] != '\0')
+		{
+			str[i] = f(i, s[i]);
+			i++;
+		}
+		return (str);
+	}
+	return (NULL);
+}
