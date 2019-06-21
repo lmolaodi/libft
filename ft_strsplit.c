ace7ebc197e34cd35f06211e249bf250f2a722eb
Lehlogonolo Morgan MOLAODI <lmolaodi@c5r9s8.wethinkcode.co.za>
2019-06-21 10:47:33 +0200

    fixed function

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: lmolaodi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 10:08:40 by lmolaodi          #+#    #+#             */
-/*   Updated: 2019/06/19 16:13:10 by lmolaodi         ###   ########.fr       */
+/*   Updated: 2019/06/21 10:45:20 by lmolaodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,22 @@ char	**ft_strsplit(char const *s, char c)
 
 	i = 0;
 	j = 0;
-	if (s)
+	if (!s || !(str = (char **)malloc(sizeof(char *) * (ft_strlen(s) + 1))))
+		return (0);
+	while (s[i] != '\0')
 	{
-		if (!(str = (char **)malloc(sizeof(char *) * (ft_strlen(s) + 1))))
-			return (0);
-		while (s[i] != '\0')
+		if (s[i] == c)
+			i++;
+		else
 		{
-			if (s[i] == c)
-				i++;
-			else
-			{
-				len = 0;
-				while (s[len + i] && (s[len + i] != c))
-					len++;
-				str[j] = ft_strsub(s, i, len);
-				i = i + len;
-				j++;
-			}
+			len = 0;
+			while (s[len + i] && (s[len + i] != c))
+				len++;
+			str[j] = ft_strsub(s, i, len);
+			i = i + len;
+			j++;
 		}
-		str[j] = 0;
-		return (str);
 	}
-	return (0);
+	str[j] = 0;
+	return (str);
 }
