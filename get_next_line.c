95acf6a5fbfa2a4fa0e76918fe3016d6a16714e9
Lehlogonolo Morgan MOLAODI <lmolaodi@c5r9s8.wethinkcode.co.za>
2019-07-02 11:08:52 +0200

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,69 @@
 /*   By: lmolaodi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/29 14:40:09 by lmolaodi          #+#    #+#             */
-/*   Updated: 2019/06/29 16:16:20 by lmolaodi         ###   ########.fr       */
+/*   Updated: 2019/07/02 11:08:00 by lmolaodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "get_next_line.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int	ft_check_fd_error(int fd, char **str, char **line)
+{
+	int i;
+
+	i = 0;
+	if (fd == -1 || line == NULL)
+		return (-1);
+	if (!*str)
+	{
+		if (!(*str = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+			return (-1);
+	}
+	return (0);
+}
+
+char	*ft_read_fd_line(char *str, int fd)
+{
+	char	file[BUFF_SIZE + 1];
+	int		i;
+	
+	while ((i = read(fd, file, BUFF_SIZE)) > 0)
+	{
+		file[i] = '\0';
+		str = ft_strjoin(str, file);
+	}
+	return (str);
+}
 
 int	get_next_line(const int fd, char **list)
 {
-	char *str;
-	char *buff;
-	int temp;
-	ssize_t i;
-	int len;
+	char	*str;
+	int		i;
 
-	if (fd < 0 || (!str[fd]  && (!str = ft_strnew(1))) || !list)
+	i = 0;
+	if (ft_check_fd_error(fd, &str, list) == -1)
 		return (-1);
-	while (!ft_strchr(str[fd], '\n') && (i == read(fd, buff, BUFF_SIZE)) > 0 )
+	if (*str)
+		ft_strcpy(*list, str);
+	str = ft_read_fd_line(str, fd);
+	if (str[i])
 	{
-		buff[i] = '\0';
-		temp = str[fd];
-		str[fd] = ft_strjoin(str[fd] , buff);
-		ft_srdel(&temp);
+		while (str[i] != '\n' && str[i])
+			i++;
+		if (i == 0)
+			(*list) = ft_strdup("");
+		else
+		{
+			(*list) = ft_strsub(str, 0, i);
+			str = &str[i + 1];
+		}
+		return (1);
 	}
-	if (str == -1 || !*(temp = str[fd]))
-		return (str == -1 ? -1 : 0);
-	if (( len = (ft_strchr(str[fd] , '\n') > 0)))
-		*line = ft_strsub(str[fd], 0, ft_strchr(str[fd], '\n') - str[fd]);
 	else
-		*line = ft_strdup(str[fd]);
-	str[fd] = ft_strsub(str[fd], (unsigned int)(ft_strlen(*line), (size_t)(ft_strlen(str[fd]) - (ft_strlen(*line) + len)));
-			ft_strdel(&temp);
-			return (!(str[fd] && !(ft_strlen(*line))));
+		(*list) = ft_strdup("");
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,14 @@
 /*   By: lmolaodi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/29 16:22:02 by lmolaodi          #+#    #+#             */
-/*   Updated: 2019/06/29 16:23:15 by lmolaodi         ###   ########.fr       */
+/*   Updated: 2019/07/01 12:00:44 by lmolaodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+#include "libft.h"
+# define BUFF_SIZE 42
 
 int	get_next_line(const int fd, char **line);
 
