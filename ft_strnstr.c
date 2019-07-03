112d026e6a65a8067a686b85d480f31bd7605791
Lehlogonolo Morgan MOLAODI <lmolaodi@c5r9s8.wethinkcode.co.za>
2019-07-03 15:22:54 +0200

    fixed strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: lmolaodi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:15:02 by lmolaodi          #+#    #+#             */
-/*   Updated: 2019/06/20 16:00:09 by lmolaodi         ###   ########.fr       */
+/*   Updated: 2019/07/03 15:22:19 by lmolaodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t j;
 
 	i = 0;
-	if (!needle[0] || haystack == needle)
+	if (!needle || haystack == needle)
 		return ((char *)haystack);
-	while (haystack[i] != '\0')
+	while (haystack[i] != '\0' && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j] && (i + j + 1) < len)
+		while (haystack[i + j] == needle[j] && (i + j) < len)
 			j++;
 		if (needle[j] == '\0')
 			return ((char *)haystack + i);
